529afd3c7a018d28080030fb4f002c688ee1f8d7
Laura Prout <laura.c.prout@gmail.com>
2021-06-05 09:39:57 -0500

    revised css and script to include displaying definitions when word arrays are full

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const $inputField = document.querySelector("[name=word");
 const $form = document.querySelector(".madlibs-form");
 const $submitButton = document.querySelector(".submit-word");
 const $clearButton = document.querySelector(".reset-game");
-const $container = document.querySelector(".word-container");
+const $container = document.querySelector(".temp-container");
+const $storyContainer = document.querySelector(".story-container");
 
 const $noun = [];
 const $adj = [];
@@ -18,9 +19,16 @@ var adjs = [];
 var adv = "";
 var wordInput = null;
 var partOfSpeech = null;
+var definition = null;
 
-var counter = 0;
-var randNum = Math.floor(Math.random()*3) + 1 // used to randomly select a story
+var randNum;
+
+function setRandNum() {
+	randNum = Math.floor(Math.random()*3) + 1 // used to randomly select a story
+	console.log(randNum);
+}
+
+setRandNum();
 
 function getInputFieldValue() {
 	return $inputField.value.trim().replaceAll("<", "").replaceAll(">","");
@@ -29,13 +37,14 @@ function getInputFieldValue() {
 function displayStory() {
 	var story;
 	if (randNum === 1) {
-		story = `Jessie and her best friend Dave went to Disney World today! They saw a ${nouns[0]} in a show at the Magic Kingdom and ate a ${adjs[0]} feast for dinner. The next day I ran ${adv} to meet Mickey Mouse in his ${nouns[1]} and then that night I gazed at the 1000 ${adjs[1]}fireworks shooting from the ${nouns[2]}.`;
+		story = `Jessie and her best friend Dave went to Disney World today! They saw a <strong>${nouns[0]}</strong> in a show at the Magic Kingdom and ate a <strong>${adjs[0]}</strong> feast for dinner. The next day I ran <strong>${adv}</strong> to meet Mickey Mouse in his <strong>${nouns[1]}</strong> and then that night I gazed at the 1000 <strong>${adjs[1]}</strong> fireworks shooting from the <strong>${nouns[2]}</strong>.`;
 	} else if (randNum === 2) {
-		story = `One day I went to the park with my ${nouns[0]}. It was very a cold and ${adjs[0]} day, but I wanted to see the ${nouns[1]}s crawling ${adv} across the grass. Suddenly, a group of ${nouns[2]} jumped out of the trees and I ran back to my ${adjs[1]} home.`;
+		story = `One day I went to the park with my <strong>${nouns[0]}</strong>. It was very a cold and <strong></strong>${adjs[0]} day, but I wanted to see the <strong></strong>${nouns[1]}s crawling <strong></strong>${adv} across the grass. Suddenly, a group of <strong></strong>${nouns[2]}s jumped out of the trees and I ran back to my <strong>${adjs[1]}</strong> home.`;
 	} else if (randNum === 3) {
-		story = "blank"
+		story = `Yesterday Jamie and their friend Ann went to the zoo and saw the smallest <strong>${nouns[0]}</strong> they had ever seen! They ate a <strong></strong>${adjs[0]} picnic before <strong>${adv}</strong> being interrupted by <strong>${adjs[1]} ${nouns[1]}s</strong> and <strong>${nouns[2]}s</strong>.`;
 	}
-}
+	$storyContainer.innerHTML += `<h3>Your random story: </h3> <p>${story}</p>` ;
+} 
 
 function formSumbitted(event) {
 	event.preventDefault();
@@ -65,54 +74,47 @@ function formSumbitted(event) {
 
 	fetch(url)
 		.then(response => response.json())
-		.then(data => {
-			// Active code is for fetching Merriam-Webster API data 
-			if (data[0].hwi === undefined) {
+		.then(data => { 
+			if (data[0].shortdef[0] === undefined) {
 				alert("That's not a word!");
 			} else {
 				wordInput = data[0].hwi.hw;  
 				partOfSpeech = data[0].fl;
-				console.log(wordInput);
-				console.log(partOfSpeech);
+				definition = data[0].shortdef[0];
 			}
 		});
 
-
+	// conditions to execute if input is of the correct type
 	if (inputFieldValue.length > 0 && wordInput != undefined) {
-	counter += 1;
-	$container.innerHTML = `
-	<li data-target="${counter}">
-		${inputFieldValue}
-		<button data-target="${counter}">Delete Word</button>
-	</li>`;
 
 		if (partOfSpeech === "noun" || partOfSpeech === "adjective" || partOfSpeech === "adverb") {
 
-		if (partOfSpeech === "noun" && nouns.length < 3) {
-			nouns.push(inputFieldValue);
-		} else if (partOfSpeech === "noun" && nouns.length === 3 ) {
-			alert("You have already entered enough nouns!");
-		} else if (partOfSpeech === "adjective" && adjs.length < 2) {
-			adjs.push(inputFieldValue);
-		} else if (partOfSpeech === "adjective" && adjs.length === 2) {
-			alert("You have already entered enough adjectives!");
-		} else if (partOfSpeech === "adverb" && adv == "") {
-			adv = inputFieldValue;
-			$adv1.innerHTML += inputFieldValue;
-		} else if (partOfSpeech === "adverb" && adv != "") {
-			alert("You have already entered an adverb.")
-		}
+			if (partOfSpeech === "noun" && nouns.length < 3) {
+				nouns.push(inputFieldValue);
+			} else if (partOfSpeech === "noun" && nouns.length === 3 ) {
+				alert("You have already entered enough nouns!");
+			} else if (partOfSpeech === "adjective" && adjs.length < 2) {
+				adjs.push(inputFieldValue);
+			} else if (partOfSpeech === "adjective" && adjs.length === 2) {
+				alert("You already entered enough adjectives!");
+			} else if (partOfSpeech === "adverb" && adv === "") {
+				adv = inputFieldValue;
+				$adv1.innerHTML += inputFieldValue + " " + definition;
+			} else if (partOfSpeech === "adverb" && adv != "") {
+				alert("You already entered an adverb.")
+			}
+
 		} else {
 		alert("Improper word type. try again!");
 		}
 
-		if (nouns.length == 3 && adjs.length == 2 && adv != "") {
+		if (nouns.length === 3 && adjs.length === 2 && adv != "") {
 
 		for (var i = 0; i < 3; i++) {
-			$noun[i].innerHTML += `${nouns[i]}`;
+			$noun[i].innerHTML += `${nouns[i]}, ${definition}`;
 		} 
 		for (var k = 0; k < 2; k++) {
-				$adj[k].innerHTML += `${adjs[k]}`;
+				$adj[k].innerHTML += `${adjs[k]}, ${definition}`;
 			} 
 
 		displayStory();
@@ -130,21 +132,10 @@ $form.addEventListener("submit", formSumbitted);
 function resetGame(event) {
 	event.preventDefault();
 	$container.innerHTML = "";
+	setRandNum();
 }
 
 $clearButton.addEventListener("click", resetGame);
-
-function deleteItem(event) {
-	const srcElement = event.srcElement;
-	if(srcElement.nodeName === "BUTTON") {
-		alert("Delete Word " + srcElement.dataset.target);
-		document.querySelector(
-			`li[data-target="${srcElement.dataset.target}"]`
-		).outerHTML = "";
-	}
-}
-
-$container.addEventListener("click", deleteItem);
 
 function inputValueChanged() {
 	var value = getInputFieldValue();
